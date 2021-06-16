7415c54f64b4a4368a10cbcd9c09404954f79802
Matthew Lynch <s191314@students.aie.edu.au>
2021-06-16 12:22:25 +0930

    Working on animation

--- Matthews_Programming_Final/Assets/Scripts/SceneLoader.cs
+++ Matthews_Programming_Final/Assets/Scripts/SceneLoader.cs
@@ -9,4 +9,10 @@ public class SceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+   
+    public void ExitGame()
+    {
+        Debug.Log("You Quit the game");
+        Application.Quit();
+    }
 }
---
+++ Matthews_Programming_Final/Assets/Scripts/StartAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator _myAnimationController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("player"))
+        {
+            _myAnimationController.SetBool("playAnimation", true);
+        }
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("player"))
+        {
+            _myAnimationController.SetBool("playAnimation", true);
+        }
+
+    }
+}
